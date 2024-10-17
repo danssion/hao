9fbbfef9137a659d231fe67ae975cad718a376ae
ecs-mongo <danssion@sina.com>
2024-10-17 12:12:45 +0800

    update up sh

--- bin/update.sh
+++ bin/update.sh
@@ -5,12 +5,26 @@ sh_path=$(cd $(dirname "$0") && pwd)
 cd $sh_path"/../"
 pwd
 
+echo "Pulling latest code from git..."
+
+
+echo "Do you want to pull the latest changes from the repository? (yes/no)"
+read pull_decision
+if [[ "$pull_decision" == "yes" || "$pull_decision" == "y" ]]; then
+    echo "Pulling latest code from git..."
+    git pull
+    echo "Git pull complete."
+else
+    echo "Skipping git pull."
+fi
+
+
 echo "正在停止容器..."
 # 停止容器
-docker-compose stop nginx server
+docker stop server
 echo "正在删除容器..."
 # 删除容器
-docker-compose rm -f nginx server
+docker-compose rm -f server
 
 echo "正在重新构建服务镜像..."
 # 重新构建后端服务镜像
@@ -18,4 +32,5 @@ docker-compose build server
 
 echo "正在重启容器..."
 # 重新启动容器
-docker-compose up -d nginx server
+docker-compose up -d server
+docker restart nginx
